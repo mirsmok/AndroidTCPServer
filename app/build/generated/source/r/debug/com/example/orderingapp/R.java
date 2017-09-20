2204d5acbb5fed75b361cff6209618152ee358b4
mirsmok <mirsmok@wp.pl>
2017-09-20 12:41:11 +0200

    Added styles

--- app/build/generated/source/r/debug/com/example/orderingapp/R.java
+++ app/build/generated/source/r/debug/com/example/orderingapp/R.java
@@ -17,6 +17,9 @@ public final class R {
     public static final class drawable {
         public static final int beer=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int menu_button=0x7f020002;
+        public static final int menu_left_button=0x7f020003;
+        public static final int red_button=0x7f020004;
     }
     public static final class id {
         public static final int LayoutClients=0x7f08000a;
@@ -51,8 +54,17 @@ public final class R {
     public static final class style {
         /**  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f050000;
+        /**  All customizations that are NOT specific to a particular API-level can go here. 
+         */
         public static final int AppTheme=0x7f050001;
+        public static final int button_text=0x7f050002;
+        public static final int menu_button=0x7f050003;
     }
 }
